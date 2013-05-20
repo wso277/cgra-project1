360d40da3d01504fc6f814fe0964ee7cc34a1b20
Wilson Oliveira <wily1993@gmail.com>
2013-05-20 15:53:11 +0100

    normals almost complete

--- src/MyRobotDraw.cpp
+++ src/MyRobotDraw.cpp
@@ -27,33 +27,35 @@ MyRobotDraw::MyRobotDraw(int stacks) {
 	int j = 13;
 
 	this->stacks = stacks;
-	points.push_back(new Point(initx,altura,initz, stacks));
+	points.push_back(new Point(initx, altura, initz, stacks));
 
 	for (int i = 1; i < 26; i++) {
 		switch (mov) {
 		case 0:
 			initx += delta;
-			points.push_back(new Point(initx,altura,initz, stacks));
+			points.push_back(new Point(initx, altura, initz, stacks));
 			break;
 
 		case 1:
 			initz -= delta;
-			points.push_back(new Point(initx,altura,initz, stacks));
+			points.push_back(new Point(initx, altura, initz, stacks));
 			break;
 
 		case 2:
 			initx -= delta;
-			points.push_back(new Point(initx,altura,initz, stacks));
+			points.push_back(new Point(initx, altura, initz, stacks));
 			break;
 
 		case 3:
 			initz += delta;
-			points.push_back(new Point(initx,altura,initz, stacks));
+			points.push_back(new Point(initx, altura, initz, stacks));
 			break;
 
 		case 4:
-			points.push_back(new Point(sin(angle*deg2rad4) / 4.0, 1.0, cos(angle*deg2rad4) / 4.0, stacks));
-			angle+=step;
+			points.push_back(
+					new Point(sin(angle * deg2rad4) / 4.0, 1.0,
+							cos(angle * deg2rad4) / 4.0, stacks));
+			angle += step;
 			break;
 		}
 
@@ -71,104 +73,124 @@ MyRobotDraw::MyRobotDraw(int stacks) {
 
 	//printf("size: %d", points.size());
 
-	for (int i=0; i<13;i++) {
+	for (int i = 0; i < 13; i++) {
 
 		points[i]->setStep(*points[j]);
 		j++;
+
+		points[i]->xtemp = points[i]->x;
+		points[i]->ztemp = points[i]->z;
 	}
 
-	
 	float alturatmp = 1.0 / stacks;
 	float altura1 = 1.0 / stacks;
 	vector<float> ponto;
-	vector<vector<float>> vertices;
+	vector<vector<float> > vertices;
 
-	for (float j=0.0; j < 1;) {
+	for (float j = 0.0; j < 1;) {
 
-		for (int i=0; i<13;i++) {
+		for (int i = 0; i < 13; i++) {
 
-		
-			if( (i % 3 ) == 0)
-				
-				switch(i)
-			{
-				case 0:
-					ponto.push_back(points[i]->xtemp - points[i]->distx);
-					ponto.push_back(alturatmp);
-					ponto.push_back(points[i]->ztemp - points[i]->distz);
+			if (j == 0) {
+				ponto.push_back(points[i]->xtemp - points[i]->distx);
+				ponto.push_back(alturatmp);
+				ponto.push_back(points[i]->ztemp - points[i]->distz);
+				vertices.push_back(ponto);
+				ponto.clear();
 
-					vertices.push_back(ponto);
-					ponto.clear();
+				ponto.push_back(points[i + 1]->xtemp - points[i + 1]->distx);
+				ponto.push_back(alturatmp);
+				ponto.push_back(points[i + 1]->ztemp - points[i + 1]->distz);
+				vertices.push_back(ponto);
+				ponto.clear();
 
-					
-					ponto.push_back(points[i]->xtemp);
+				ponto.push_back(points[i + 1]->xtemp - points[i + 1]->distx);
+				ponto.push_back(alturatmp - altura1);
+				ponto.push_back(points[i + 1]->ztemp - points[i + 1]->distz);
+				vertices.push_back(ponto);
+				ponto.clear();
+
+				ponto.push_back(points[i]->xtemp);
+				ponto.push_back(alturatmp - altura1);
+				ponto.push_back(points[i]->ztemp);
+				vertices.push_back(ponto);
+				ponto.clear();
+
+				if (i > 0) {
+					ponto.push_back(points[i - 1]->xtemp);
 					ponto.push_back(alturatmp - altura1);
-					ponto.push_back(points[i]->ztemp);
-
+					ponto.push_back(points[i - 1]->ztemp);
 					vertices.push_back(ponto);
 					ponto.clear();
-					break;
+				}
 
-				case 3:
-				case 6:
-				case 9:
-				case 12:
-					vector<float> tmp1,tmp2;
-					ponto.push_back(points[i]->xtemp - points[i]->distx);
-					ponto.push_back(alturatmp);
-					ponto.push_back(points[i]->ztemp - points[i]->distz);
-					tmp1 = ponto;
-					vertices.push_back(ponto);
-					ponto.clear();
+				normais.push_back(calculaNormais(vertices));
+				vertices.clear();
+			} else {
+				ponto.push_back(points[i]->xtemp - points[i]->distx);
+				ponto.push_back(alturatmp);
+				ponto.push_back(points[i]->ztemp - points[i]->distz);
+				vertices.push_back(ponto);
+				ponto.clear();
 
-					
-					ponto.push_back(points[i]->xtemp);
+				ponto.push_back(points[i + 1]->xtemp - points[i + 1]->distx);
+				ponto.push_back(alturatmp);
+				ponto.push_back(points[i + 1]->ztemp - points[i + 1]->distz);
+				vertices.push_back(ponto);
+				ponto.clear();
+
+				ponto.push_back(points[i + 1]->xtemp - points[i + 1]->distx);
+				ponto.push_back(alturatmp - altura1);
+				ponto.push_back(points[i + 1]->ztemp - points[i + 1]->distz);
+				vertices.push_back(ponto);
+				ponto.clear();
+
+				ponto.push_back(points[i]->xtemp);
+				ponto.push_back(alturatmp - altura1);
+				ponto.push_back(points[i]->ztemp);
+				vertices.push_back(ponto);
+				ponto.clear();
+
+				ponto.push_back(points[i]->xtemp + points[i + 1]->distx);
+				ponto.push_back(alturatmp - (2 * altura1));
+				ponto.push_back(points[i]->ztemp + points[i + 1]->distz);
+				vertices.push_back(ponto);
+				ponto.clear();
+
+				if (i > 0) {
+					ponto.push_back(points[i - 1]->xtemp);
 					ponto.push_back(alturatmp - altura1);
-					ponto.push_back(points[i]->ztemp);
-					tmp2 = ponto;
+					ponto.push_back(points[i - 1]->ztemp);
 					vertices.push_back(ponto);
 					ponto.clear();
-					
-					normais.push_back(calculaNormais(vertices));
 
-					
-					vertices.clear();
+					ponto.push_back(
+							points[i - 1]->xtemp + points[i - 1]->distx);
+					ponto.push_back(alturatmp - (2 * altura1));
+					ponto.push_back(
+							points[i - 1]->ztemp + points[i - 1]->distz);
+					vertices.push_back(ponto);
+					ponto.clear();
 
-					vertices.push_back(tmp1);
-					vertices.push_back(tmp2);
-					break;
+					ponto.push_back(points[i - 1]->xtemp);
+					ponto.push_back(alturatmp - altura1);
+					ponto.push_back(points[i - 1]->ztemp);
+					vertices.push_back(ponto);
+					ponto.clear();
+				}
+				normais.push_back(calculaNormais(vertices));
+				vertices.clear();
 			}
-
-			
-
-			
-		
 
 			points[i]->xtemp -= points[i]->distx;
 			points[i]->ztemp -= points[i]->distz;
 
 		}
 
-	
-
-		alturatmp+=altura1;
-		j+=altura1;
+		alturatmp += altura1;
+		j += altura1;
 
 	}
-
-	for (int i=0; i<13;i++) {
-
-
-		points[i]->xtemp = points[i]->x;
-		points[i]->ztemp = points[i]->z;
-	}
-
-
-
-	
-
-
-	
 
 }
 
@@ -190,270 +212,116 @@ void MyRobotDraw::draw() {
 	float altura1 = 1.0 / stacks;
 	int orientacao = 0;
 	vector<float> ponto;
-	vector<vector<float>> vertices;
+	vector<vector<float> > vertices;
 
 //	printf("normal0 = %f\nnormal\ = %f\nnormal2 = %f\n", normais[0][0],normais[0][1],normais[0][2]);
 
-	for (float j=0.0; j < 1;) {
-
-		glBegin(GL_TRIANGLE_STRIP);
-		for (int i=0; i<13;i++) {
-
-		
-			
-			
-				glNormal3f(normais[0][0], normais[0][1], normais[0][2]);
-				
-	
-			
-			glVertex3f(points[i]->xtemp - points[i]->distx, altura, points[i]->ztemp - points[i]->distz );
-
-			
-			glVertex3f(points[i]->xtemp, altura - altura1, points[i]->ztemp );
-
-			
-		
-
-			points[i]->xtemp -= points[i]->distx;
-			points[i]->ztemp -= points[i]->distz;
-
-			if( (i % 3) == 0 && i != 0) orientacao++;
-
-		}
-
-		glEnd();
-
-		altura+=altura1;
-		j+=altura1;
-
-	}
-
-	for (int i=0; i<13;i++) {
-
+	for (int i = 0; i < 13; i++) {
 
 		points[i]->xtemp = points[i]->x;
 		points[i]->ztemp = points[i]->z;
 	}
 
+	for (float j = 0.0; j < 1;) {
+
+		int a = 0;
+		glBegin(GL_TRIANGLE_STRIP);
+		for (int i = 0; i < 13; i++) {
+
+
+
+			glNormal3f(normais[a+13][0], normais[a+13][1], normais[a+13][2]);
+
+			glVertex3f(points[i]->xtemp - points[i]->distx, altura,
+					points[i]->ztemp - points[i]->distz);
+
+			glNormal3f(normais[a][0], normais[a][1], normais[a][2]);
+			a++;
+			glVertex3f(points[i]->xtemp, altura - altura1, points[i]->ztemp);
+
+			points[i]->xtemp -= points[i]->distx;
+			points[i]->ztemp -= points[i]->distz;
+
+			if ((i % 3) == 0 && i != 0)
+				orientacao++;
+
+		}
+
+		glEnd();
+
+		altura += altura1;
+		j += altura1;
+
+	}
+
 }
 
-//for (float j = 0; j < 1;) {
-//	glBegin(GL_TRIANGLE_STRIP);
-//
-//	glNormal3f(x, y, z);
-//
-//	if (j != 1 - altura1) {
-//		glVertex3f(initx + (deltax / 2), altura, initz - (deltax / 2));
-//	} else {
-//		glNormal3f(cos(angle * deg2rad4) / 4, 0, sin(angle * deg2rad4) / 4);
-//		glVertex3f(cos(angle * deg2rad4) / 4, altura,
-//				sin(angle * deg2rad4) / 4);
-//		angle += step;
-//	}
-//
-//	glNormal3f(x, y, z);
-//	glVertex3f(initx, altura - altura1, initz);
-//
-//	for (int i = 1; i <= slices; i++) {
-//
-//		switch (mov) {
-//		case 0:
-//			glNormal3f(x, y, z);
-//			initx += delta;
-//			if (j != 1 - altura1) {
-//				if (i == 3) {
-//					if (initx <= 0)
-//						glVertex3f(initx + (deltax / 2), altura,
-//								initz - (deltax / 2));
-//					else
-//						glVertex3f(initx - (deltax / 2), altura,
-//								initz - (deltax / 2));
-//				} else
-//					glVertex3f(initx, altura, initz - (deltax / 2));
-//			} else {
-//				glNormal3f(cos(angle * deg2rad4) / 4, 0,
-//						sin(angle * deg2rad4) / 4);
-//				glVertex3f(cos(angle * deg2rad4) / 4, altura,
-//						sin(angle * deg2rad4) / 4);
-//				angle += step;
-//			}
-//
-//			glNormal3f(x, y, z);
-//			glVertex3f(initx, altura - altura1, initz);
-//			break;
-//
-//		case 1:
-//			glNormal3f(z, y, x);
-//			initz -= delta;
-//			if (j != 1 - altura1) {
-//				if (i == 6) {
-//					if (initz <= 0)
-//						glVertex3f(initx - (deltax / 2), altura,
-//								initz + (deltax / 2));
-//					else
-//						glVertex3f(initx - (deltax / 2), altura,
-//								initz - (deltax / 2));
-//				} else
-//					glVertex3f(initx - (deltax / 2), altura, initz);
-//			} else {
-//				glNormal3f(cos(angle * deg2rad4) / 4, 0,
-//						sin(angle * deg2rad4) / 4);
-//				glVertex3f(cos(angle * deg2rad4) / 4, altura,
-//						sin(angle * deg2rad4) / 4);
-//				angle += step;
-//			}
-//
-//			glNormal3f(z, y, x);
-//			glVertex3f(initx, altura - altura1, initz);
-//			break;
-//
-//		case 2:
-//			glNormal3f(x, y, -z);
-//			initx -= delta;
-//			if (j != 1 - altura1) {
-//				if (i == 9) {
-//					if (initx <= 0)
-//						glVertex3f(initx + (deltax / 2), altura,
-//								initz + (deltax / 2));
-//					else
-//						glVertex3f(initx - (deltax / 2), altura,
-//								initz + (deltax / 2));
-//				} else
-//					glVertex3f(initx, altura, initz + (deltax / 2));
-//
-//			} else {
-//				glNormal3f(cos(angle * deg2rad4) / 4, 0,
-//						sin(angle * deg2rad4) / 4);
-//				glVertex3f(cos(angle * deg2rad4) / 4, altura,
-//						sin(angle * deg2rad4) / 4);
-//				angle += step;
-//			}
-//
-//			glNormal3f(x, y, -z);
-//			glVertex3f(initx, altura - altura1, initz);
-//			break;
-//
-//		case 3:
-//			glNormal3f(-z, y, x);
-//			initz += delta;
-//			if (j != 1 - altura1) {
-//				if (i == 12) {
-//					if (initz <= 0)
-//						glVertex3f(initx + (deltax / 2), altura,
-//								initz + (deltax / 2));
-//					else
-//						glVertex3f(initx + (deltax / 2), altura,
-//								initz - (deltax / 2));
-//				} else
-//					glVertex3f(initx + (deltax / 2), altura, initz);
-//
-//			} else {
-//				glNormal3f(cos(angle * deg2rad4) / 4, 0,
-//						sin(angle * deg2rad4) / 4);
-//				glVertex3f(cos(angle * deg2rad4) / 4, altura,
-//						sin(angle * deg2rad4) / 4);
-//				angle += step;
-//			}
-//
-//			glNormal3f(-z, y, x);
-//			glVertex3f(initx, altura - altura1, initz);
-//			break;
-//		}
-//
-//		if (i == 3)
-//			mov = 1;
-//		if (i == 6)
-//			mov = 2;
-//		if (i == 9)
-//			mov = 3;
-//	}
-//
-//	glEnd();
-//
-//	mov = 0;
-//	initx += (deltax / 2);
-//	initz -= (deltax / 2);
-//	j += altura1;
-//	altura += altura1;
-//	c -= deltax;
-//	delta = c / 3.0;
-//
-//}
+vector<float> MyRobotDraw::calculaNormais(vector<vector<float> > vertices) {
+	vector<float> tmp;
+	tmp.push_back(0);
+	tmp.push_back(0);
+	tmp.push_back(0);
 
-/*float altura = 1.0 / stacks;
- float altura1 = 1.0 / stacks;
- float c = 1.0;
- float delta = c / 3.0;
- float deltax = 0.5 / stacks;
- float initx = -0.5;
- float initz = 0.5;
- float angle = 225;
- float step = 360.0 / slices;
- float divStep = 4.0 / stacks;
- float div = 2.0 - divStep;
- int mov = 0;
+	for (unsigned int i = 0; i < vertices.size(); i++) {
+		vector<float> current = vertices[i];
+		vector<float> next = vertices[(i + 1) % vertices.size()];
 
+		tmp[0] += ((current[1] - next[1]) * (current[2] + next[2]));
+		tmp[1] += ((current[2] - next[2]) * (current[0] + next[0]));
+		tmp[2] += ((current[0] - next[0]) * (current[1] + next[1]));
 
- for (float j = 0; j < 1;) {
+	}
 
- glBegin(GL_TRIANGLE_STRIP);
+	printf("normal0 = %f\nnormal\ = %f\nnormal2 = %f\n", tmp[0], tmp[1],
+			tmp[2]);
 
+	return tmp;
+}
 
- glVertex3f(cos(angle * deg2rad4) / (div + divStep), altura,
- sin(angle * deg2rad4) / (div + divStep));
+/*
+ if ((i % 3) == 0)
 
- if (j == 0.0) {
- glVertex3f(initx, 0, initz);
- } else {
- glVertex3f(cos(angle * deg2rad4) / div, altura - altura1,
- sin(angle * deg2rad4) / div);
- }
-
- angle += step;
- for (int i = 0; i <= slices; i++) {
-
- glVertex3f(cos(angle * deg2rad4) / (div + divStep), altura,
- sin(angle * deg2rad4) / (div + divStep));
-
- if (j == 0.0) {
- switch (mov) {
+ switch (i) {
  case 0:
- initx += delta;
- glVertex3f(initx, 0, initz);
+ ponto.push_back(points[i]->xtemp - points[i]->distx);
+ ponto.push_back(alturatmp);
+ ponto.push_back(points[i]->ztemp - points[i]->distz);
+
+ vertices.push_back(ponto);
+ ponto.clear();
+
+ ponto.push_back(points[i]->xtemp);
+ ponto.push_back(alturatmp - altura1);
+ ponto.push_back(points[i]->ztemp);
+
+ vertices.push_back(ponto);
+ ponto.clear();
  break;
- case 1:
- initz -= delta;
- glVertex3f(initx, 0, initz);
- break;
- case 2:
- initx -= delta;
- glVertex3f(initx, 0, initz);
- break;
+
  case 3:
- initz += delta;
- glVertex3f(initx, 0, initz);
+ case 6:
+ case 9:
+ case 12:
+ vector<float> tmp1, tmp2;
+ ponto.push_back(points[i]->xtemp - points[i]->distx);
+ ponto.push_back(alturatmp);
+ ponto.push_back(points[i]->ztemp - points[i]->distz);
+ tmp1 = ponto;
+ vertices.push_back(ponto);
+ ponto.clear();
+
+ ponto.push_back(points[i]->xtemp);
+ ponto.push_back(alturatmp - altura1);
+ ponto.push_back(points[i]->ztemp);
+ tmp2 = ponto;
+ vertices.push_back(ponto);
+ ponto.clear();
+
+ normais.push_back(calculaNormais(vertices));
+
+ vertices.clear();
+
+ vertices.push_back(tmp1);
+ vertices.push_back(tmp2);
  break;
- }
-
- if (i == 4)
- mov = 1;
- if (i == 5)
- mov = 2;
- if (i == 8)
- mov = 3;
-
- } else {
- glVertex3f(cos(angle * deg2rad4) / div, altura - altura1,
- sin(angle * deg2rad4) / div);
- }
-
- angle+=step;
- }
-
- glEnd();
-
- angle = 225;
- div += divStep;
- altura+= altura1;
- j+=altura1;
  }*/
